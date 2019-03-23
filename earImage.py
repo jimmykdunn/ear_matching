13914e993ca7c079437b0d8948799129286f7f16
jimmykdunn <jkdunn@bu.edu>
2019-03-23 12:35:43 -0400

    Skeleton code plus very basic pixel-by-pixel SSD comparison

---
+++ earImage.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+# Dharmit Dalvi and James Dunn, Spring 2019, Boston University
+# Code written for CS 640 (Artificial Intelligence) project.
+import cv2
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+# Image type enumeration
+FIRST = 0  # no donut, first image (no extension)
+SECOND = 1 # no donut, 2nd image ('t' extension)
+DONUT1 = 2 # donut, first image ('d' extension)
+DONUT2 = 3 # donut, 2nd image ('dt' extension)
+
+DISPLAY_SHAPE = (504,672)  # 1/6 raw image size
+
+# Class for holding an ear image
+class earImage:
+    def __init__(self, file):
+        # Identifying information
+        self.nameString = file.split(os.sep)[-1].split('.')[0]
+        self.number = int(self.nameString.split('_')[0])
+        self.typeStr = self.nameString.split('_')[-1]
+        if self.typeStr == '':
+            self.type = FIRST
+        if self.typeStr == 't':
+            self.type = SECOND
+        if self.typeStr == 'd':
+            self.type = DONUT1
+        if self.typeStr == 'dt':
+            self.type = DONUT2
+        
+        # Read the image
+        self.rawImage = cv2.imread(file)
+        self.nx, self.ny, self.ncolors = self.rawImage.shape
+        
+        # Processed versions
+        self.rgbImage = cv2.cvtColor(self.rawImage, cv2.COLOR_BGR2RGB)
+        self.scaled = []
+        self.aligned = []
+        self.backgroundRemoved = []
+        
+    def displayRawRGB(self, shape=DISPLAY_SHAPE, time=0):
+        #plt.imshow(self.rgbImage)
+        forDisplay = cv2.resize(self.rawImage, shape)
+        cv2.imshow(self.nameString, forDisplay)
+        cv2.waitKey(time)
+        cv2.destroyWindow(self.nameString)
+        
+    def scale(self):
+        pass
+    
+    def align(self):
+        pass
+    
+    def removeBackground(self):
+        pass
+    
+    # Run preprocessing on the ear image
+    def preprocess(self):
+        self.removeBackground()
+        self.align()
+        self.scale()
+    
+    # Compare this image to another image, get a score for it
+    def compare(self, other):
+        # This function runs a (yet to be determined) algorithm to compare
+        # the current image to another image (other)
+        
+        # Super basic pixel-by-pixel sum-squared difference. Standin for
+        # real algorithms.
+        score = 1.0 / (1.0 + np.mean( \
+                (self.rawImage/np.mean(self.rawImage) - \
+                 other.rawImage/np.mean(other.rawImage))**2))
+        return score
+
--- main.py
+++ main.py
@@ -3,20 +3,75 @@
 # Code written for CS 640 (Artificial Intelligence) project.
 import cv2
 import numpy as np
+import glob
+import os
+import matplotlib.pyplot as plt
+
+import earImage
 
 # Global parameters
 DATA_PATH = "data" # folder, relative to run location, where data is stored
 
 
-# Main function
-def main():
+# Reads every image
+def readImages():
     # Loop over the images, reading them in
+    images = [] # all the images
+    filelist = glob.glob(DATA_PATH + "/*jpg*")
+    print("Reading images from file")
+    for file in filelist:
+        image = earImage.earImage(file) # read and initialize the image
         
         # Preprocess the image
+        image.preprocess()
         
-        # Run matching algorithm on all the other images
+        # FOR TESTING ONLY
+        #image.displayRawRGB(time=1000) # display image for 1s
+        
+        images.append(image) # add the image to the list of all images
+        
+        print("Successfully read image: ", image.nameString)
+        
+        # TO MAKE TESTING QUICKER. REMOVE FOR FINAL RUNS.
+        # Stop after reading in the first 40 for testing more quickly
+        if len(images) >= 16:
+            break
+    
+    return images
 
-    pass
+
+# Calculates the similarity between each pair of images
+def calculateSimilarity(images):
+    # Pairwise image similarity measure in an NxN matrix
+    similarityMatrix = np.zeros([len(images),len(images)])
+        
+    # Loop over each image, then see if it matches the rest
+    for i1, image in enumerate(images):
+        print("Calculating similarities for image ", i1)
+        for i2, image2 in enumerate(images):
+            score = image.compare(image2)
+            similarityMatrix[i1,i2] = score
+    
+    return similarityMatrix
+
+
+# Main function
+def main():
+    # Read in all the images
+    images = readImages()
+        
+    # Calculate the pairwise similarity between images
+    similarityMatrix = calculateSimilarity(images)
+    
+    # Display the similarity matrix as an image
+    cv2.imshow("Similarity matrix", cv2.resize(similarityMatrix, (400,400)))
+    cv2.waitKey(0)
+    cv2.destroyWindow("Similarity matrix")
+    
+    
+    # Return similarity matrix and images
+    return similarityMatrix, images
+
 
 # Actually execute the program
-main()
+similarityMatrix, images = main()
