08f01ae65a5794793b3e6ae3afd71395c9441d58
jimmykdunn <jkdunn@bu.edu>
2019-03-22 21:07:24 -0400

    Adding empty main.py file

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# Dharmit Dalvi and James Dunn, Spring 2019, Boston University
+# Code written for CS 640 (Artificial Intelligence) project.
+import cv2
+import numpy as np
+
+# Global parameters
+DATA_PATH = "data" # folder, relative to run location, where data is stored
+
+
+# Main function
+def main():
+    # Loop over the images, reading them in
+        
+        # Preprocess the image
+        
+        # Run matching algorithm on all the other images
+
+    pass
+
+# Actually execute the program
+main()
